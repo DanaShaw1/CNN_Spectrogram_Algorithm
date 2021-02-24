e55d7c3d8ae3dd11f227b2e545aecb7fbaea7d6d
Dana Shaw <danashaw@bu.edu>
2021-02-24 14:17:02 -0500

    Code to extract EEG data from .dat files

---
+++ Extract EEG/extract_EEG.m
@@ -0,0 +1,32 @@
+%Extract the EEG data from a .dat file
+clear
+clc
+
+filename = 'UMHS-0018-0002.day-04.lay';
+
+%Get header from .lay file (also need corresponding .dat file to run this
+%line)
+header = lay_hdr_read(filename, 0);
+Fs = header.samplingrate;
+
+%Total number of data points
+total_data_pts = header.datapoints;
+
+%get data for the first 1000 seconds
+data = lay_data_read( header, 1, Fs * 1000 );
+
+%Extract data specified channels
+d1 = data(11,:);
+d2 = data(12,:);
+
+dEDF = -(d1 - d2);
+
+%Time axis
+tEDF = (1:size(dEDF,2))/Fs;
+
+%Plot data
+plot(tEDF,dEDF)
+
+%Save the data into a new file
+newfile = 'UMHS-0018-0002.day-04_BD1-BD2.mat';
+save(newfile,'dEDF', 'tEDF');
