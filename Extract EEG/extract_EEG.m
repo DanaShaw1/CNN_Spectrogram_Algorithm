53653030a8d54390759b8df2f46ea5a4a4c61f94
Dana Shaw <danashaw@bu.edu>
2021-03-03 16:24:37 -0500

    Fixed code so it extracts full time of desired bipolar reference

--- Extract EEG/extract_EEG.m
+++ Extract EEG/extract_EEG.m
@@ -12,22 +12,51 @@ Fs = header.samplingrate;
 %Total number of data points
 total_data_pts = header.datapoints;
 
-%get data for the first 1000 seconds
-data = lay_data_read( header, 1, Fs * 1000 );
+start_idx = 1;
+end_idx = Fs * 100;
+increment = Fs*100;
+done = false;
+dEDF = [];
 
-%Extract data specified channels
-d1 = data(11,:);
-d2 = data(12,:);
+while ~done
+    if end_idx > total_data_pts
+        done = true;
+        end_idx = total_data_pts;
+    end
+    
+    %get data for the all channles in increments
+    data = lay_data_read( header, start_idx, end_idx );
+    
+    %Extract data specified channels
+    d1 = data(21,:);
+    d2 = data(22,:);
+    
+    %Clear the data variable
+    clear data
+    
+    %Get the difference between the 2 channels
+    d1d2_diff = d1 - d2;
+    
+    %Concatenate the extracted channels to the rest
+    dEDF = horzcat(dEDF, d1d2_diff);
+    
+    %Increment the indicies
+    start_idx = start_idx + increment;
+    end_idx = end_idx + increment;
+    disp(end_idx);
+end
 
-%dEDF = transpose(data(41,:));
-dEDF = transpose(d1 - d2);
+
+%Transpose the data
+dEDF = transpose(dEDF);
 
 %Time axis
 tEDF = transpose((1:size(dEDF,1))/Fs);
 
 %Plot data
 plot(tEDF,dEDF)
+xlabel('Time (s)')
 
 %Save the data into a new file
-newfile = 'UMHS-0018-0002.day-04_BD1-BD2_transpose.mat';
+newfile = 'UMHS-0018-0002.day-04_CD1-CD2.mat';
 save(newfile,'dEDF', 'tEDF');
